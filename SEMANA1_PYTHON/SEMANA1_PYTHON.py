de1a9986ad7145945dc914698bfb95bffac5d421
Adrián García <mgga989307277@gmail.com>
2025-09-09 11:41:49 -0500

    ejer3 sem1

--- SEMANA1_PYTHON/SEMANA1_PYTHON.py
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -5,5 +5,15 @@ def ejer1():
     print(f"\n{nombre}, bienvenido al curso de fudamentos de algoritmo de la carrera {carrera}")
 def ejer2():
     print("\"Ruben\"")
+def ejer3():
+    num1 = int(input("Ingresre número 1: "))
+    num2 = int(input("Ingresre número 2: "))
 
-ejer2()
+    print("Suma: ", (num1+num2))
+    print("Resta: ", (num1-num2))
+    print("Multiplicacón: ", (num1*num2))
+    print("División: ", (num1/num2))
+
+
+
+ejer3()
